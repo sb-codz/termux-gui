ce2917152eef3c0daf9e7424223fed14ed18c6ec
sb-codz <smaxwap@gmail.com>
2022-04-06 21:07:26 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,17 +33,20 @@ DIR=$HOME/termux-gui/codz;
   _pkgs=(tigervnc xfce4 xfce4-terminal xfce-theme-manager xfce4-appfinder xfce4-clipman-plugin xfce4-places-plugin xfce4-notes-plugin xfce4-goodies desktop-file-utils audacious leafpad pavucontrol-qt hexchat geany xarchiver thunar unzip otter-browser rofi gzip wget parole )
   setup_vnc_xfce_pkg() {
 	echo -e ${RED}"\n[*] Installing Termux Desktop..."
+	echo -e "    _______________ ${CHECK_MARK} _______________"
 	echo -e ${CYAN}"\n[*] Updating Termux Base... \n"
 	{ reset_color; pkg autoclean; pkg upgrade -y; }
+	echo -e "    _______________ ${CHECK_MARK} _______________"
 	echo -e ${CYAN}"\n[*] Enabling Termux X11-repo... \n"
 	{ reset_color; pkg install -y x11-repo; }
+		echo -e "    _______________ ${CHECK_MARK} _______________"
 	echo -e ${CYAN}"\n[*] Installing required programs... \n"
 	for package in "${_pkgs[@]}"; do
 		{ reset_color; pkg install -y "$package"; }
 		_ipkg=$(pkg list-installed $package 2>/dev/null | tail -n 1)
 		_checkpkg=${_ipkg%/*}
 		if [[ "$_checkpkg" == "$package" ]]; then
-			echo -e ${GREEN}"\n[*] Package $package installed successfully.\n"
+			echo -e ${GREEN}"\n[${CHECK_MARK}] Package $package installed successfully.\n"
 			continue
 		else
 			echo -e ${MAGENTA}"\n[!] Error installing $package, Terminating...\n"
@@ -69,33 +72,37 @@ DIR=$HOME/termux-gui/codz;
   #Extanal
   setup_data() {
   termux-setup-storage
-  echo "Backup Config data..."
+  echo "${CYAN}Backup Config data..."
   rm -rf $HOME/.backup
   mkdir $HOME/.backup
   cp $HOME/.config $HOME/.backup
   cp $HOME/.vnc $HOME/.backup
   cd $HOME
   sleep 1
-	#  Remove HOME Folders
-  echo "Remove data..."
+  	echo -e "    _______________ ${CHECK_MARK} _______________"
+ ## Remove HOME Folders
+  echo "${CYAN}Remove Folders data..."
 	rm -rf $HOME/.local
 	rm -rf $HOME/.themes
 	rm -rf $HOME/.icons
-	# rm -rf $HOME/.vnc
+	## rm -rf $HOME/.vnc
 	sleep 1
+	echo -e "    _______________ ${CHECK_MARK} _______________"
 	# Remove Bin Files & Folders
-  echo "Remove data..."
+  echo "${CYAN}Remove Files data..."
 	rm $PREFIX/bin/desktop
 	#rm $PREFIX/bin/vnc-config
 	#rm $PREFIX/bin/vnc-autostart-config
 	sleep 1
+	echo -e "    _______________ ${CHECK_MARK} _______________"
 	# Coppy Folders & files
   DIR=$HOME/termux-gui/codz
-	echo "Coppy data..."
+	echo "${CYAN}Coppy data..."
 	echo "$HOME/termux-gui/codz/desk.sh" > $PREFIX/bin/desktop
   sleep 1
+  echo -e "    _______________ ${CHECK_MARK} _______________"
   # Make folders
-  echo "Make Folders data..."
+  echo "${GREEN}Make Folders data..."
 	mkdir $HOME/Desktop
 	mkdir $HOME/Downloads
 	mkdir $HOME/Templates
@@ -106,20 +113,23 @@ DIR=$HOME/termux-gui/codz;
 	mkdir $HOME/.themes
 	mkdir $HOME/.icons
 	sleep 1
+	echo -e "    _______________ ${CHECK_MARK} _______________"
 	#link folder 
-	echo "Link storage data..."
+	echo "${CYAN}Link storage data..."
   ln -s $HOME/storage/music Music
   sleep 1
+  echo -e "    _______________ ${CHECK_MARK} _______________"
   }
   # Permission
   x_permission() {
-  echo "Enable Permission data..."
+  echo "${GREEN}Enable Permission data..."
   chmod +x $PREFIX/bin/desktop
   chmod +x $HOME/termux-gui/codz/desk.sh
 	chmod +x ~/.vnc/xstartup
+	echo -e "    _______________ ${CHECK_MARK} _______________"
 	}
 	post_msg () {
-	echo "Install Is Done. :) \n NOTE\: Dont Remove \" termux-gui\" Folders and Files. \n To Start Desktop Write Comand \" desktop \" Then Hit Entet And Input Options"
+	echo -e "${ORANGE}Install Is Done. :) \n\n ${RED}NOTE: ${GREEN}Dont Remove ${RED}\" termux-gui\" ${GREEN}Folders and Files. \n To Start Desktop Write Comand ${ORANGE}\" desktop \" \n ${CYAN}Then Hit Enter And Input Options"
 	}
 	## Install Termux Desktop
 install_tg() {
@@ -136,7 +146,7 @@ uninstall_td() {
 	echo -e ${RED}"\n[*] Unistalling Termux Desktop..."
 	echo -e ${CYAN}"\n[*] Removing Packages..."
 	for package in "${_pkgs[@]}"; do
-		echo -e ${GREEN}"\n[*] Removing Packages ${ORANGE}$package \n"
+		echo -e ${GREEN}"\n[${CHECK_MARK}] Removing Packages ${ORANGE}$package \n"
 		{ reset_color; apt-get remove -y --purge --autoremove $package; }
 	done
 	echo -e ${RED}"\n[*] Termux Desktop Unistalled Successfully.\n"
